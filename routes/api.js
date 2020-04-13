047ef6ac1e16ab8d65f45949b314ad4d4988f6e0
CKanishka <ckanishka24@gmail.com>
2020-04-14 01:25:56 +0530

    client side routing fixed

--- routes/api.js
+++ routes/api.js
@@ -6,5 +6,7 @@ let data = JSON.parse(fs.readFileSync('response.json', 'utf-8'))
 router.get('/getdata',(req,res)=>{
     res.status(200).json({data}) 
 })
-
+router.get('/*', function(req, res) {
+    res.sendFile(`${__dirname}/build/index.html`);
+})
 module.exports=router;
