01603623eae5e0df5943590f31c6d9fc270b579b
CKanishka <ckanishka24@gmail.com>
2020-04-13 19:58:50 +0530

    table added

---
+++ src/components/Table.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const renderTableData = (data) => {
+    return data.map((row) => {
+    let cols = ["conceptName","statType","uniqueCount","nullCount"]
+    return (
+        <tr key={row["cid"]}>
+            {cols.map((col,index)=> {
+                if(col==="conceptName")
+                    return (<td key={index}><a href={`/chart?cid=${row["cid"]}`}>{row[col]}</a></td>)
+                return (
+                    <td key={index}>{row[col]}</td>
+                    )
+                })
+            }
+        </tr>
+        )
+    })
+}
+const renderTableHeader = (headings) => {
+    return headings.map((item, index) => (<th key={index}>{item}</th>))
+ }
+
+const Table = (props) => {
+
+    return (
+        <div className="table">
+            <table id="table1">
+               <tbody>
+                  <tr>{renderTableHeader(props.headings)}</tr>
+                  {renderTableData(props.data)}
+               </tbody>
+            </table>
+         </div>
+    );
+}
+
+export default Table
